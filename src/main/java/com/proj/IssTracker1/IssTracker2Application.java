e05f7ab843c5698c27be1a09d9f39881c70637c6
Tirthankar <cooltiru15@gmail.com>
2024-02-15 22:37:10 +0530

    added new

---
+++ src/main/java/com/proj/IssTracker1/IssTracker2Application.java
@@ -0,0 +1,13 @@
+package com.proj.IssTracker1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssTracker2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IssTracker2Application.class, args);
+	}
+
+}
